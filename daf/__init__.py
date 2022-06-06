5010f7eaacd5d67430e21229ffbb4aed5bb73ca8
orenbenkiki <oren@ben-kiki.org>
2022-06-06 22:17:53 +0300

    Add test for minimal python version.

--- daf/__init__.py
+++ daf/__init__.py
@@ -8,15 +8,21 @@ Data in Axes in Formats
     ``pandas`` APIs to deal with this.
 """
 
-import sys
+# See https://github.com/jwilk/python-syntax-errors
+# pylint: disable=using-constant-test,missing-function-docstring,pointless-statement
+if 0:
+
+    async def function(value):
+        f"{await value}"  # Python >= 3.7 is required
+
 
 __author__ = "Oren Ben-Kiki"
 __email__ = "oren@ben-kiki.org"
 __version__ = "0.1.0-dev.1"
 
-
 # pylint: disable=wrong-import-position
 
+import sys
 
 from .storage import *
 from .typing import *
